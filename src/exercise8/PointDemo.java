475f45349809cfe8f9320e833a011c73ed760456
IV202 <>
2021-12-31 14:52:07 +0000

    ubung8IV202

--- src/exercise8/PointDemo.java
+++ src/exercise8/PointDemo.java
@@ -20,7 +20,7 @@ public class PointDemo {
         System.out.println("Point 5: " + point5);
         System.out.println("Point 6: " + point6);
 
-        point2.translate(4, 5);
+              point2.translate(4, 5);
         System.out.println("Translated point 2: " + point2);
         System.out.println("Translated point 6: " + point6);
 
